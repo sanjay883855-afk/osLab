b36cff9479b8ddd580a5f26adb0d9de88d979408
sanjay883855-afk <sanjay883855@gmail.com>
2026-09-18 02:26:41 -0500

    Delete exp1.sh

--- exp1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "===== System Information ====="
-echo "Hostname : $(hostname)"
-echo "User     : $(whoami)"
-echo "Kernel   : $(uname -r)"
-echo "Date     : $(date)"
-echo "Uptime   :"
-uptime
